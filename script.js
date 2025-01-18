a2df665d01768090d8aed74adb8bd77beff8cb2c
saisharanreddymanne <148990776+saisharanreddymanne@users.noreply.github.com>
2025-01-18 01:04:20 -0600

    Added Back_to_top button at right corner of portfolio.

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Show/hide Back to Top button on scroll
+window.onscroll = function () {
+    const button = document.getElementById("backToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  };
+  
+  // Scroll to the top when the button is clicked
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scrolling
+    });
+  }
